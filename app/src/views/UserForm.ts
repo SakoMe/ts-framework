2cf7142f2a82b766eba306afe986404623783d9b
SakoMe <melkonsa@gmail.com>
2021-10-30 11:55:05 +0400

    set up the binding between event and views

--- app/src/views/UserForm.ts
+++ app/src/views/UserForm.ts
@@ -1,19 +1,43 @@
 export class UserForm {
 	constructor(private parent: Element) {}
 
+	eventsMap: { [key: string]: () => void } = {
+		'click:button': this.onButtonClick,
+		'mouseenter:h1': this.onHeaderHover
+	};
+
+	onButtonClick(): void {
+		console.log('button clicked');
+	}
+
+	onHeaderHover(): void {
+		console.log('header hovered');
+	}
+
 	template(): string {
 		return `
       <div>
         <h1>User Form</h1>
         <input />
+        <button>Click Me</button>
       </div>
     `;
 	}
 
-	render(): void {
-		const termplateElement = document.createElement('template');
-		termplateElement.innerHTML = this.template();
+	bindEvents(fragment: DocumentFragment): void {
+		const { eventsMap } = this;
+		for (const eventKey in eventsMap) {
+			const [eventName, selector] = eventKey.split(':'); // eventName -> 'click', selector -> 'button'
+			fragment.querySelectorAll(selector).forEach((el) => {
+				el.addEventListener(eventName, eventsMap[eventKey]);
+			});
+		}
+	}
 
-		this.parent.append(termplateElement.content);
+	render(): void {
+		const templateElement = document.createElement('template');
+		templateElement.innerHTML = this.template();
+		this.bindEvents(templateElement.content);
+		this.parent.append(templateElement.content);
 	}
 }
