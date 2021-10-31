e0e989c78c71af48a3b011846416a5a3b3ead129
SakoMe <melkonsa@gmail.com>
2021-10-31 13:27:37 +0400

    testing out functionality

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,29 +1,27 @@
-// import { User } from './models/User';
-// import { UserEdit } from './views/UserEdit';
-
-// const user = User.buildUser({ name: 'Sarkis', age: 30 });
-
-// const root = document.getElementById('root');
-
-// if (root) {
-// 	const userEdit = new UserEdit(root, user);
-// 	userEdit.render();
-
-// 	console.log(userEdit);
-// } else {
-// 	throw new Error('Root element not found');
-// }
-
 import { UserList } from './views/UserList';
 import { Collection } from './models/Collection';
 import { UserProps, User } from './models/User';
+import { UserEdit } from './views/UserEdit';
+
+const user = User.buildUser({ name: 'Sarkis', age: 44 });
+
+const root = document.getElementById('edit');
+
+if (root) {
+	const userEdit = new UserEdit(root, user);
+	userEdit.render();
+
+	console.log(userEdit);
+} else {
+	throw new Error('Root element not found');
+}
 
 const users = new Collection('http://localhost:3000/users', (json: UserProps) =>
 	User.buildUser(json)
 );
 
 users.on('change', () => {
-	const root = document.getElementById('root');
+	const root = document.getElementById('users');
 
 	if (root) new UserList(root, users).render();
 });
